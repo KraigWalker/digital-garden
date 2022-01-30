0124fa16e0796a002b1c92d54d6b68b2b6a63853
Kraig Walker <kraig_walker@me.com>
2022-01-30 13:27:41 +0000

    Update about/index.tsx to test Nx Cloud Report

--- apps/site/pages/about/index.tsx
+++ apps/site/pages/about/index.tsx
@@ -6,7 +6,7 @@ export interface AboutProps {}
 export function About(props: AboutProps) {
   return (
     <div>
-      <h1>Welcome to About!</h1>
+      <h1>Welcome to About Kraig!</h1>
     </div>
   );
 }
